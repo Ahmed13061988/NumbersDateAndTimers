60c9c39c37a9ba43a16fd06e7fe2130ded7d1e93
Ahmed Hussein <wolktheline@yahoo.com>
2022-10-18 22:44:34 -0600

    timestamp

--- script.js
+++ script.js
@@ -412,4 +412,6 @@ console.log(new Date('December 24, 2015'));
 
 console.log(new Date(account1.movementsDates[0]));
 console.log(new Date(2037, 10, 15, 23, 5));
-console.log(new Date(2037, 10, 31));
+console.log(new Date(2037, 10, 31)); // JS auto correction
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
