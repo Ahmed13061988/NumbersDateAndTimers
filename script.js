15ce2ad04ed806502c1739489b11d914dac84c96
Ahmed Hussein <wolktheline@yahoo.com>
2022-10-29 20:16:40 -0600

    adding date to the date lable

--- script.js
+++ script.js
@@ -162,6 +162,8 @@ containerApp.style.opacity = 100;
 
 const now = new Date();
 
+labelDate.textContent = now;
+
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
@@ -269,55 +271,55 @@ console.log(+'23');
 
 //Parsing
 
-console.log(Number.parseInt('30px'));
+// console.log(Number.parseInt('30px'));
 
-console.log(Number.parseFloat('2.5rem'));
-console.log(Number.parseInt('2.5rem'));
+// console.log(Number.parseFloat('2.5rem'));
+// console.log(Number.parseInt('2.5rem'));
 
 //check if value is not a number(NaN)
-console.log(Number.isNaN(20));
-console.log(Number.isNaN(+'20X'));
-console.log(Number.isNaN(20 / 0));
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(20 / 0));
 
 //checking if the value is a number
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(20 / 0));
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(20 / 0));
 
-console.log(Number.isInteger('20'));
-console.log(Number.isInteger(20));
-console.log(Number.isInteger('20X'));
-console.log(Number.isInteger(20 / 0));
-console.log(Number.isInteger(2.2));
+// console.log(Number.isInteger('20'));
+// console.log(Number.isInteger(20));
+// console.log(Number.isInteger('20X'));
+// console.log(Number.isInteger(20 / 0));
+// console.log(Number.isInteger(2.2));
 
-console.log(Math.sqrt(34));
-console.log(8 ** (1 / 3));
+// console.log(Math.sqrt(34));
+// console.log(8 ** (1 / 3));
 
-console.log(Math.max(1, 4, 7, 3, 9));
-console.log(Math.max(1, 4, 7, 3, 9, '23'));
-console.log(Math.max(1, 4, 7, 3, 9, '23px'));
-console.log(Math.min(1, 2, 3, 4, 9, 0));
+// console.log(Math.max(1, 4, 7, 3, 9));
+// console.log(Math.max(1, 4, 7, 3, 9, '23'));
+// console.log(Math.max(1, 4, 7, 3, 9, '23px'));
+// console.log(Math.min(1, 2, 3, 4, 9, 0));
 
-console.log(Math.PI);
-console.log(Math.PI * parseInt('10px') ** 2);
+// console.log(Math.PI);
+// console.log(Math.PI * parseInt('10px') ** 2);
 
-console.log(Math.trunc(Math.random() * 6 + 1));
+// console.log(Math.trunc(Math.random() * 6 + 1));
 
 const randomInt = (min, max) =>
   Math.trunc(Math.random() * (max - min) + 1) + min;
 
-console.log(randomInt(10, 20));
+// console.log(randomInt(10, 20));
 
 //Rounding integers
-console.log(Math.trunc(23.33));
+// console.log(Math.trunc(23.33));
 
-console.log(Math.round(23.55));
-console.log(Math.ceil(23.98));
+// console.log(Math.round(23.55));
+// console.log(Math.ceil(23.98));
 
-console.log(Math.floor(23.9));
+// console.log(Math.floor(23.9));
 
-console.log((2.3).toFixed(0));
-console.log(+(2.3).toFixed(3));
+// console.log((2.3).toFixed(0));
+// console.log(+(2.3).toFixed(3));
 
 //Numeric separators
 
