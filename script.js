fcb61b6507953f6fab17cb50ac401d6f7fb22f21
Ahmed Hussein <wolktheline@yahoo.com>
2022-10-31 19:38:45 -0600

    adding Math.abs

--- script.js
+++ script.js
@@ -490,7 +490,8 @@ const past = new Date(2012, 11, 5, 20);
 
 console.log(+future); // --> THIS WILL BE CONVERTED INTO MILLISECONDS
 
-const calcDaysDiff = (date1, date2) => (date2 - date1) / (1000 * 60 * 60 * 24);
+const calcDaysDiff = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
 
 const days1 = calcDaysDiff(new Date(2037, 3, 14), new Date(2037, 3, 24));
 console.log(days1);
