c6500937c9901c35dfcabaac7388c084ede7e084
Ahmed Hussein <wolktheline@yahoo.com>
2022-10-31 19:37:31 -0600

    finding the passing days between two dates

--- script.js
+++ script.js
@@ -486,5 +486,11 @@ findIndex(array);
 //Calculation date
 
 const future = new Date(2037, 11, 15, 20);
+const past = new Date(2012, 11, 5, 20);
 
 console.log(+future); // --> THIS WILL BE CONVERTED INTO MILLISECONDS
+
+const calcDaysDiff = (date1, date2) => (date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysDiff(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
