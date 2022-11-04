09302961bb9fc15315b534baf397b8731a6b2b9e
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-03 18:28:42 -0600

    second

--- script.js
+++ script.js
@@ -574,4 +574,4 @@ const options1 = {
 // console.log('Syria:', new Intl.NumberFormat('ar-SY', options1).format(num));
 // console.log('US:', new Intl.NumberFormat(navigator.language).format(num));
 
-setTimeout(() => console.log('Here is your pizza 🍕'));
+setTimeout(() => console.log('Here is your pizza 🍕'), 3000);
